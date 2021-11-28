3dce59ac5e055b8a251ddab65504a7455f279824
monish karunakaran <ckmonish2000@gmail.com>
2021-11-28 15:12:30 +0530

    init

--- app.py
+++ app.py
@@ -8,5 +8,12 @@ async def root():
 
 
 @app.get('/posts/{item}')
-async def root(item):
+async def root(item : int):
   return {"message":item}
+
+
+# body 
+
+@app.post('/posts')
+async def root(obj):
+  return {"message":obj}
