0ffd5291a97945d494156ce30b81e2b7784196a9
monish karunakaran <ckmonish2000@gmail.com>
2021-11-28 19:38:11 +0530

    working docker and fast api ref

--- app.py
+++ app.py
@@ -2,8 +2,22 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import Optional
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 
 @app.get('/')
 async def root():
--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
 
 
 function App() {
+
+  const click = () => {
+    fetch('http://localhost:5001')
+      .then(v => v.json())
+      .then(v => { console.log(v) })
+      .catch(err => console.log(err))
+  }
+
   return (
     <div className="App">
-      <h1> hello</h1>
+      <button onClick={click}>tests</button>
     </div>
   );
 }
